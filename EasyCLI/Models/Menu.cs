913e7ed43a90a91950a0e1771c6acb9cf7da41b8
nebelfox <mashtaliaryaroslav@gmail.com>
2021-10-03 15:41:07 +0300

    Menu refactor
    - Now contains root Namespace and delegates execution to it.
    - Has Help method, that shows the menu help message,
      that consists of title, header,
      root namespace help message and footer.
    - Handles ArgumentException during execution,
      that may be thrown by command/namespace/action
      and shows it's message.
    - Run renamed to StartDialogue.
    - StartDialogue prints greeting at the beginning,
      and farewell, when the menu is terminated.
    - Is relocated to the root project namespace
      in usage convenience purposes.

---
+++ EasyCLI/Models/Menu.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EasyCLI.Models.Styles;
+using EasyCLI.Parsers;
+
+// ReSharper disable once CheckNamespace
+namespace EasyCLI
+{
+    using Context = Models.Context;
+    
+    public sealed class Menu
+    {
+        private const char Escape = '\\';
+        private const char Separator = ' ';
+        private readonly OptionsParser _optionsParser;
+        private readonly Models.Executables.Namespace _commands;
+        private readonly Style _style;
+        private readonly string _greeting;
+        private readonly string _farewell;
+        private bool _isRunning;
+
+        public Menu(OptionsParser optionsParser,
+                      string help,
+                      Namespace rootNamespace,
+                      Style style,
+                      string greeting,
+                      string farewell)
+        {
+            _optionsParser = optionsParser;
+            _style = style;
+            _commands = rootNamespace.Build(style, help);
+            _greeting = greeting;
+            _farewell = farewell;
+        }
+
+        public void StartDialogue()
+        {
+            _isRunning = true;
+            Console.WriteLine(_greeting);
+            Console.Write(_style.Separator);
+            while (_isRunning)
+            {
+                Console.Write(_style.PromptIndicator);
+                
+                var inputs = new LinkedList<string>();
+                do
+                {
+                    inputs.AddLast(Console.ReadLine());
+                }
+                while (inputs.Last.Value.EndsWith(Escape));
+
+                var input = string.Join(Separator, inputs.Select(i => i.TrimEnd(Escape)));
+                
+                try
+                {
+                    Execute(input);
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    Console.Write(_style.Separator);
+                }
+            }
+
+            Console.WriteLine(_farewell);
+        }
+
+        public void Execute(string command)
+        {
+            IEnumerable<string> tokens = command.Split(Separator, 
+                                                       StringSplitOptions.RemoveEmptyEntries);
+            Options options = _optionsParser.Parse(tokens);
+            var context = new Context(this, options);
+            _commands.Execute(context);
+        }
+
+        public void Terminate()
+        {
+            _isRunning = false;
+        }
+
+        public void Help()
+        {
+            _commands.Help();
+        }
+    }
+}
